3f25af21a390dd2ac22cda8e8ac9dbe3a7382178
Xavier308 <xavvis308@gmail.com>
2024-05-08 12:42:59 -0400

    pep8 correction

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,12 +2,12 @@
 def islower(c):
     """
     Check if a character is lowercase.
-    
+
     Args:
         c: A character (string of length 1).
-        
+
     Returns:
         True if c is lowercase, False otherwise.
     """
-    # Check if the ASCII value of c is between the ASCII values of lowercase 'a' and 'z'
+    # Check the ASCII value of c 
     return ord('a') <= ord(c) <= ord('z')
