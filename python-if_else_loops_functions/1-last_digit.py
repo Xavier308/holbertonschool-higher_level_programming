324ed9bc6990b4182b715d08605e5f091a3f51f4
Xavier308 <xavvis308@gmail.com>
2024-05-08 18:13:37 -0400

    negative sign position changed

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = number % 10 if number >= 0 else -number % 10
+last_digit = number % 10 if number >= 0 else number % -10
 
 if last_digit > 5:
     print(f"Last digit of {number} is {last_digit} and is greater than 5")
