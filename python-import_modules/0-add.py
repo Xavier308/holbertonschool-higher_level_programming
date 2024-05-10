42d7df415ec9e106ba559b0782aea6a126381041
Xavier308 <xavvis308@gmail.com>
2024-05-10 19:13:39 -0400

    update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 
-if __name__ == "__main__":
+def main():
     a = 1
     b = 2
     result = add(a, b)
-    print(f"{a} + {b} = {result}")
+    print(f"{a} - {b} = {result}")
+
+if __name__ == "__main__":
+    main()
