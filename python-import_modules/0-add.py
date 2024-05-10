8d6caf34dec2213d78684c68b610ed249a8e94dd
Xavier308 <xavvis308@gmail.com>
2024-05-10 19:51:59 -0400

    pycodestyle

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 from add_0 import add
 
+
 def main():
     a = 1
     b = 2
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
+
 
 if __name__ == "__main__":
     main()
