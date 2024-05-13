3e0b4724cfd39f79c09999b64b22973652762cc7
Xavier308 <xavvis308@gmail.com>
2024-05-13 16:31:04 -0400

    2. Replace element

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    """
+    Replaces an element in a list at a specified position if the index is valid.
+    Returns the original list if the index is invalid.
+    """
+    if 0 <= idx < len(my_list):  # Check if index is within the valid range
+        my_list[idx] = element
+    return my_list
