3689c18a28a82d66f234df2b07193d34e06a3579
Xavier308 <xavvis308@gmail.com>
2024-05-15 15:17:12 -0400

    len() removed

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,25 +3,26 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
+            # Check if the current element is an integer
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         except IndexError:
+            # Break out of the loop if index is out of range
             break
-    print()
+    print()  # Ensures a newline after finishing printing
     return count
 
 
 if __name__ == "__main__":
-    # test
+    # Test cases
     my_list = [1, 2, 3, 4, 5]
     nb_print = safe_print_list_integers(my_list, 2)
     print("nb_print: {:d}".format(nb_print))
 
     my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-    nb_print = safe_print_list_integers(my_list, len(my_list))
+    nb_print = safe_print_list_integers(my_list, 7)
     print("nb_print: {:d}".format(nb_print))
 
-    # Handle IndexError properly by breaking the loop
-    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
+    nb_print = safe_print_list_integers(my_list, 9)
     print("nb_print: {:d}".format(nb_print))
