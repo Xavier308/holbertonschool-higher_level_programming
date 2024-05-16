d22a5a6d4936ba07e75ad957e9b69d9ac0f70279
Xavier308 <xavvis308@gmail.com>
2024-05-16 09:18:35 -0400

    deleted main lines

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -11,17 +11,3 @@ def safe_print_list_integers(my_list=[], x=0):
             return (IndexError) # break was used before 
     print()
     return count
-
-
-if __name__ == "__main__":
-    # Test cases
-    my_list = [1, 2, 3, 4, 5]
-    nb_print = safe_print_list_integers(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-
-    my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-    nb_print = safe_print_list_integers(my_list, 7)
-    print("nb_print: {:d}".format(nb_print))
-
-    nb_print = safe_print_list_integers(my_list, 9)
-    print("nb_print: {:d}".format(nb_print))
