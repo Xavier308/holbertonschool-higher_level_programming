24b035ead0f3ecd707b28625769188ebd5c2c8bc
Xavier J. Cruz <xavvis308@gmail.com>
2024-05-22 20:47:24 -0400

    Update 1-my_list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -17,15 +17,3 @@ class MyList(list):
         altering the original list.
         """
         print(sorted(self))
-
-
-if __name__ == "__main__":
-    my_list = MyList()
-    my_list.append(1)
-    my_list.append(4)
-    my_list.append(2)
-    my_list.append(3)
-    my_list.append(5)
-    print(my_list)
-    my_list.print_sorted()
-    print(my_list)
