2e3c8e85e7c301217f6c0c3a4c33451d84886c31
Xavier J. Cruz <xavvis308@gmail.com>
2024-05-22 20:54:41 -0400

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -9,11 +9,3 @@ class BaseGeometry:
     An empty class.
     """
     pass
-
-
-if __name__ == "__main__":
-    bg = BaseGeometry()
-
-    print(bg)
-    print(dir(bg))
-    print(dir(BaseGeometry))
