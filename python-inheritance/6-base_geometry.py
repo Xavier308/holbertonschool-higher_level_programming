98f297522d46ca76243c105e2a16316ba0d245f1
Xavier J. Cruz <xavvis308@gmail.com>
2024-05-22 20:54:58 -0400

    Update 6-base_geometry.py

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -18,12 +18,3 @@ class BaseGeometry:
             Exception: "area() is not implemented"
         """
         raise Exception("area() is not implemented")
-
-
-if __name__ == "__main__":
-    bg = BaseGeometry()
-
-    try:
-        print(bg.area())
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
