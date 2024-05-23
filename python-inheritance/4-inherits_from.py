488df870ce623053ef1e0ade0ad0a63367d87a00
Xavier J. Cruz <xavvis308@gmail.com>
2024-05-22 20:54:15 -0400

    Update 4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -19,12 +19,3 @@ def inherits_from(obj, a_class):
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
 
-
-if __name__ == "__main__":
-    a = True
-    if inherits_from(a, int):
-        print("{} inherited from class {}".format(a, int.__name__))
-    if inherits_from(a, bool):
-        print("{} inherited from class {}".format(a, bool.__name__))
-    if inherits_from(a, object):
-        print("{} inherited from class {}".format(a, object.__name__))
