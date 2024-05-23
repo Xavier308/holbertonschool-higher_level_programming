fd1724bd5598d1884cc9481a4fa81044988527b9
Xavier J. Cruz <xavvis308@gmail.com>
2024-05-22 20:45:50 -0400

    Update 7-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -37,24 +37,3 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
-
-if __name__ == "__main__":
-    bg = BaseGeometry()
-
-    bg.integer_validator("my_int", 12)
-    bg.integer_validator("width", 89)
-
-    try:
-        bg.integer_validator("name", "John")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("age", 0)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        bg.integer_validator("distance", -4)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
