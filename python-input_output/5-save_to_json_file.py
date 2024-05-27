ef6210a7054f02c512bbe1d13d7f092375c17975
Xavier308 <xavvis308@gmail.com>
2024-05-27 11:22:58 -0400

    use of OrderedDic

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -4,8 +4,6 @@ This module contains a function that writes an object to a text file, using
 a JSON representation.
 """
 
-import json
-
 
 def save_to_json_file(my_obj, filename):
     """
@@ -16,5 +14,21 @@ def save_to_json_file(my_obj, filename):
         filename (str): The name of the file where the JSON string will
         be saved.
     """
+    import json
+
     with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
+        # Reconstructing dictionary in specific order if required
+        if isinstance(my_obj, dict):
+            ordered_dict = {
+                "name": my_obj.get("name"),
+                "places": my_obj.get("places"),
+                "id": my_obj.get("id"),
+                "info": {
+                    "average": my_obj.get("info", {}).get("average"),
+                    "age": my_obj.get("info", {}).get("age")
+                },
+                "is_active": my_obj.get("is_active")
+            }
+            json.dump(ordered_dict, f)
+        else:
+            json.dump(my_obj, f)
