8846e979858807ef135a4698ff3c98bf6fd187d7
Xavier308 <xavvis308@gmail.com>
2024-05-27 11:28:27 -0400

    code more simple

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -17,18 +17,4 @@ def save_to_json_file(my_obj, filename):
     import json
 
     with open(filename, 'w', encoding='utf-8') as f:
-        # Reconstructing dictionary in specific order if required
-        if isinstance(my_obj, dict):
-            ordered_dict = {
-                "name": my_obj.get("name"),
-                "places": my_obj.get("places"),
-                "id": my_obj.get("id"),
-                "info": {
-                    "average": my_obj.get("info", {}).get("average"),
-                    "age": my_obj.get("info", {}).get("age")
-                },
-                "is_active": my_obj.get("is_active")
-            }
-            json.dump(ordered_dict, f)
-        else:
-            json.dump(my_obj, f)
+        json.dump(my_obj, f)
