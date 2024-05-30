a0bc70f95daeb053c1eb0907bdfc095b3b1ee91f
Xavier308 <xavvis308@gmail.com>
2024-05-30 12:58:28 -0400

    handle post request

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -13,10 +13,12 @@ users = {
 def home():
     return "Welcome to the Flask API!"
 
+# /status endpoint
 @app.route('/status')
 def status():
     return "OK"
 
+# /data endpoint to get all usernames
 @app.route('/data')
 def get_usernames():
     return jsonify(list(users.keys()))
@@ -34,17 +36,23 @@ def get_user(username):
 @app.route('/add_user', methods=['POST'])
 def add_user():
     data = request.get_json()
-    username = data.get('username')
-    if not username or username in users:
-        return jsonify({"error": "Invalid or existing username"}), 400
-
+    
+    if not data or 'username' not in data:
+        return jsonify({"error": "Username is required"}), 400
+    
+    username = data['username']
+    
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 400
+    
     users[username] = {
         "username": username,
         "name": data.get('name'),
         "age": data.get('age'),
         "city": data.get('city')
     }
-    return jsonify({"message": "User added", "user": users[username]})
+    
+    return jsonify({"message": "User added", "user": users[username]}), 201
 
 
 if __name__ == "__main__":
