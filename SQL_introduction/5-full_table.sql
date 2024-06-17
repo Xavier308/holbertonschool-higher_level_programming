d2c47446aec30d5310e6304ca754f9ea6f06576e
Xavier308 <xavvis308@gmail.com>
2024-06-17 17:31:11 -0400

    #5 again

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,3 @@
 -- Full description
 -- script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY
-FROM information_schema.COLUMNS
-WHERE TABLE_NAME = 'first_table' AND TABLE_SCHEMA = 'hbtn_0c_0';
+SHOW CREATE TABLE first_table;
