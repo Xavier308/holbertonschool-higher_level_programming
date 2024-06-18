6017a2070e504b76ac820fe4e7a2eef1b83c2dff
Xavier308 <xavvis308@gmail.com>
2024-06-18 10:49:24 -0400

    include the format "<genre> - <number_of_shows>

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
---  Genre ID for all shows
--- script that lists all shows contained in the database hbtn_0d_tvshows
+-- Genre ID for all shows
+-- Script that lists all shows contained in the database hbtn_0d_tvshows
 
 -- List all shows along with their genre IDs, displaying NULL for shows without genres
 SELECT tv_shows.title, tv_show_genres.genre_id
--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,9 @@
 -- Only Comedy
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
 
-
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN genres ON genres.id = tv_show_genres.genre_id
-WHERE genres.name = 'Comedy'
--- Sorted by show title
-ORDER BY tv_shows.title ASC;
+SELECT CONCAT(genres.name, ' - ', COUNT(tv_show_genres.tv_show_id)) AS `genre - number_of_shows`
+FROM genres
+JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+GROUP BY genres.id
+HAVING COUNT(tv_show_genres.tv_show_id) > 0
+ORDER BY COUNT(tv_show_genres.tv_show_id) DESC;
