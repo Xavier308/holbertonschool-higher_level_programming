963da59dadf2badb53db054757ae419664d677e6
Xavier308 <xavvis308@gmail.com>
2024-06-18 10:52:32 -0400

    number_of_shows

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Only Comedy
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
 
-SELECT CONCAT(genres.name, ' - ', COUNT(tv_show_genres.tv_show_id)) AS `genre - number_of_shows`
+SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
 GROUP BY genres.id
