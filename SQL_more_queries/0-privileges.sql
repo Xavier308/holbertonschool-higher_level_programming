cb2d6b84771bfff38b2a608b5ecf3974917c5b0f
Xavier308 <xavvis308@gmail.com>
2024-06-18 08:51:37 -0400

    no show grants

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -13,5 +13,5 @@ GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
 
 -- List all privileges of the users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- SHOW GRANTS FOR 'user_0d_2'@'localhost';
