263b596cbdeb539713e6c23c335926764a0eb51a
Xavier308 <xavvis308@gmail.com>
2024-06-18 09:09:34 -0400

    less line of codes

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,13 +1,5 @@
 -- My privileges!
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
 
--- Attempt to display privileges for user_0d_1
-SELECT 'Privileges for user_0d_1:';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
--- Separator for clarity in output
-SELECT '--------------------------------------';
-
--- Attempt to display privileges for user_0d_2
-SELECT 'Privileges for user_0d_2:';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
