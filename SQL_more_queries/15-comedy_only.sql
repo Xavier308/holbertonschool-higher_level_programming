faf2e83f69402cd858286e578a52de991e57fd52
Xavier308 <xavvis308@gmail.com>
2024-06-18 11:21:54 -0400

    exercise 15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,9 @@
 -- Only Comedy
 -- Script that lists all Comedy shows in the database hbtn_0d_tvshows.
 
-
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-JOIN genres ON genres.id = tv_show_genres.genre_id
-WHERE genres.name = 'Comedy'
--- Sorted by show title
-ORDER BY tv_shows.title ASC;
+SELECT s.title
+FROM tv_shows s
+JOIN tv_show_genres sg ON s.id = sg.show_id
+JOIN tv_genres g ON sg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY s.title ASC;
