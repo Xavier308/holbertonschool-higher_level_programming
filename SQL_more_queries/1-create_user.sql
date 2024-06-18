9446a39c9636798d538f069f8b67f09ee3622a69
Xavier308 <xavvis308@gmail.com>
2024-06-18 09:13:40 -0400

    exercise #1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,11 @@
 -- Root user
 -- script that creates the MySQL server user user_0d_1.
+
+-- Create user 'user_0d_1' if it does not exist
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to 'user_0d_1' on all databases and tables
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Apply changes immediately
+FLUSH PRIVILEGES;
