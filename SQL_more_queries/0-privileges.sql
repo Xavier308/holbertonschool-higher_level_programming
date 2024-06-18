4bb3bd988fde19773bc9186503f9832bbcd3bcfe
Xavier308 <xavvis308@gmail.com>
2024-06-18 08:45:42 -0400

    exercise #0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,17 @@
 -- My privileges!
--- script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+-- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+
+-- Create the users
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
+
+-- Grant some privileges
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
+
+-- Flush privileges
+FLUSH PRIVILEGES;
+
+-- List all privileges of the users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
