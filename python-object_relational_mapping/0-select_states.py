a82efb5fa2ad0da3be03e9fe310ecb36702e42bc
Xavier308 <xavvis308@gmail.com>
2024-06-20 14:55:06 -0400

    pycodestyle

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
 Module to list all states from the database hbtn_0e_0_usa.
-This script takes 3 arguments: mysql username, mysql password, and database name.
+This script takes 3 arguments: mysql username, mysql password,
+and database name.
 It connects to a MySQL server running on localhost at port 3306.
 Results are sorted in ascending order by states.id and displayed.
 """
@@ -9,11 +10,18 @@ Results are sorted in ascending order by states.id and displayed.
 import MySQLdb
 import sys
 
+
 def list_states(username, password, dbname):
     """
     Connects to the database and prints all states sorted by id.
     """
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=dbname)
+    db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=dbname
+    )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
@@ -21,6 +29,7 @@ def list_states(username, password, dbname):
         print(row)
     cur.close()
     db.close()
+
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
