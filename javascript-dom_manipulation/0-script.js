bcd8347f1c1864bacd3bf8307ae556b4c25f5920
xavierdev <xavvis308@gmail.com>
2024-07-26 08:49:28 -0400

    color me

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// This script updates the text color of the header element to red (#FF0000)
+document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('header'); // Selects the first header tag
+    header.style.color = '#FF0000'; // Changes the color of the header text to red
+  });
